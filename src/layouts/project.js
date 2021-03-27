e9b3c23b355e7bd2885e01957238fe579abb6eb5
Stackbit Code Editor <projects@stackbit.com>
2021-03-27 10:03:13 +0000

    project.js: changed

--- src/layouts/project.js
+++ src/layouts/project.js
@@ -47,7 +47,7 @@ export default class Project extends React.Component {
                         <div className="post-content inner-sm">
                             {markdownify(_.get(this.props, 'page.content', null))}
                         </div>
-                        <section style={{  display: 'grid'align-items: center;
+                        <section style={{  display: 'grid', align-items: center;
   grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));}}
                         {this.state.images.map((image, i) => 
                         <a href={`https://res.cloudinary.com/ajonp/image/upload/${image.public_id}`} target="_blank">
