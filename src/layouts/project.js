057b98a0dc4a3a75ba963f6df2a20bfcda06a302
Stackbit Code Editor <projects@stackbit.com>
2021-03-27 09:41:36 +0000

    project.js: changed

--- src/layouts/project.js
+++ src/layouts/project.js
@@ -1,52 +1,53 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {Layout} from '../components/index';
-import {htmlToReact, withPrefix, markdownify} from '../utils';
+import { Layout } from '../components/index';
+import { htmlToReact, withPrefix, markdownify } from '../utils';
 
 export default class Project extends React.Component {
-      constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
-    componentDidMount(){
-                fetch('https://res.cloudinary.com/ajonp/image/list/ducks.json',
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+    componentDidMount() {
+        fetch('https://res.cloudinary.com/ajonp/image/list/ducks.json',
             {
                 mode: 'cors',
                 headers: {
                     'content-type': 'application/json'
                 }
             }).then(response => response.json())
-            .then(json => {console.log(json);
+            .then(json => {
+                console.log(json);
                 setImages(json.resources);
-                }
-                )
+            }
+            )
     }
     render() {
         return (
             <Layout {...this.props}>
-            <div className="inner outer">
-              <article className="post post-full">
-                <header className="post-header inner-sm">
-                  <h1 className="post-title line-top">{_.get(this.props, 'page.title', null)}</h1>
-                  {_.get(this.props, 'page.subtitle', null) && (
-                  <div className="post-subtitle">
-                    {htmlToReact(_.get(this.props, 'page.subtitle', null))}
-                  </div>
-                  )}
-                </header>
-                {_.get(this.props, 'page.image', null) && (
-                <div className="post-image">
-                  <img src={withPrefix(_.get(this.props, 'page.image', null))} alt={_.get(this.props, 'page.image_alt', null)} />
+                <div className="inner outer">
+                    <article className="post post-full">
+                        <header className="post-header inner-sm">
+                            <h1 className="post-title line-top">{_.get(this.props, 'page.title', null)}</h1>
+                            {_.get(this.props, 'page.subtitle', null) && (
+                                <div className="post-subtitle">
+                                    {htmlToReact(_.get(this.props, 'page.subtitle', null))}
+                                </div>
+                            )}
+                        </header>
+                        {_.get(this.props, 'page.image', null) && (
+                            <div className="post-image">
+                                <img src={withPrefix(_.get(this.props, 'page.image', null))} alt={_.get(this.props, 'page.image_alt', null)} />
+                            </div>
+                        )}
+                        <div className="post-content inner-sm">
+                            {markdownify(_.get(this.props, 'page.content', null))}
+                        </div>
+                    </article>
                 </div>
-                )}
-                <div className="post-content inner-sm">
-                  {markdownify(_.get(this.props, 'page.content', null))}
-                </div>
-              </article>
-            </div>
             </Layout>
         );
     }
